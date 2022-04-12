f16a720e1917978995ac95a284f30e44b7e8afae
s1lencef <98669592+s1lencef@users.noreply.github.com>
2022-04-12 19:24:55 +0300

    Add files via upload

---
+++ first/cmake-build-debug/CMakeFiles/prog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prog.dir/main.cpp.o"
+  "libprog.dll.a"
+  "prog.exe"
+  "prog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,66 @@
+# include <iostream>
+
+using namespace std;
+
+const int Ax = 0, Ay = 0, Bx = 5, By = 5, Cx = 0, Cy =5;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int triangle_vertex_x, triangle_vertex_y;
+    if (Ax != Ay) {
+        triangle_vertex_x = Ax;
+        triangle_vertex_y = Ay;
+    } else if (Bx != By) {
+        triangle_vertex_x = Bx;
+        triangle_vertex_y = By;
+    } else {
+        triangle_vertex_x = Cx;
+        triangle_vertex_y = Cy;
+    }
+    int triangle_type, max_height;
+    max_height = max(Ay, By);
+    max_height = max(max_height, Cy);
+    if (triangle_vertex_y < max_height) {
+        triangle_type = 1;
+    } else {
+        triangle_type = 2;
+    }
+
+    int self_x = 9, self_y = 9;
+
+    while (self_x != 0 && self_y != 0) {
+        cout << "Введите значение координаты 'х' \n";
+        cin >> self_x;
+        cout << " Введите значение координаты 'y'\n";
+        cin >> self_y;
+        if (self_x == 0 && self_y == 0){
+            break;
+        }
+        if (triangle_type == 1) {
+        int min_x;
+        min_x = min(Ax, Bx);
+        min_x = min(Cx, min_x);
+        cout << self_x << "   " << self_y << "\n";
+        if ((self_x <= triangle_vertex_x) && (self_x >= min_x) && (self_y <= max_height) && (self_y >= triangle_vertex_y) && (self_y <= self_x) ) {
+            cout << "OK\n";
+        } else
+            cout << "Не OK\n";
+
+        }
+        else {
+            int max_x, min_y;
+            max_x = max(Ax, Bx);
+            max_x = max(Cx, max_x);
+            min_y = min(Ay, By);
+            min_y = min(Cy, min_y);
+            cout << self_x << "   " << self_y << "  ";
+            if ((self_x >= triangle_vertex_x) && (self_x <= max_x) && (self_y >= min_y ) && (self_y <= triangle_vertex_y) && (self_x <= self_y) ) {
+                cout << "OK\n";
+            } else
+                cout << "Не OK\n";
+
+        }
+    }
+
+    return 0;
+}
